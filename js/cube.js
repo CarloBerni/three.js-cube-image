d92177b6251c6067edacb5144f27d51aa7ead224
CarloBerni <carlo.berni@hetic.net>
2019-03-28 11:35:04 +0100

    yes

--- js/cube.js
+++ js/cube.js
@@ -1,6 +1,6 @@
 var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    var renderer = new THREE.WebGLRenderer();
+    var renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
     camera.position.set(0,0,20);
@@ -19,16 +19,25 @@ window.addEventListener( 'resize', function ( ) {
 
 // orbit
 
-var controls = new THREE.OrbitControls( camera, renderer.domElement );
+var orbit = new THREE.OrbitControls( camera, renderer.domElement );
 
 
 // create the cube
 
-var cubeGeometry = new THREE.CubeGeometry( 1, 1, 1);
+var geometry = new THREE.CubeGeometry( 2, 2, 2);
+var cubeMaterials = [
+  new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('/textures/canonWork.jpeg'), side: THREE.DoubleSide } ),
+  new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('/textures/carlo13-min.jpeg'), side: THREE.DoubleSide } ),
+  new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('/textures/socomptoirWork.jpeg'), side: THREE.DoubleSide } ),
+  new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('/textures/ToTheTopWork.jpeg'), side: THREE.DoubleSide } ),
+  new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('/textures/socomptoirWork.jpeg'), side: THREE.DoubleSide } ),
+  new THREE.MeshBasicMaterial( { map: new THREE.TextureLoader().load('/textures/carlo13-min.jpeg'), side: THREE.DoubleSide } )
+];
 
 
-var cubeMaterial = new THREE.MeshBasicMaterial( { color: 0xFF0FAA, wireframe: false });
-var cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
+
+var material = new THREE.MeshFaceMaterial( cubeMaterials );
+var cube = new THREE.Mesh( geometry, material );
 scene.add(cube);
 
 camera.position.z = 3;
