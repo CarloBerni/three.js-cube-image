80dee17383c06b70dd1c609c3de8428ccefed2f4
CarloBerni <carlo.berni@hetic.net>
2019-03-29 14:35:03 +0100

    last

--- js/cube.js
+++ js/cube.js
@@ -13,10 +13,11 @@
 })()
 
 var scene = new THREE.Scene();
-scene.background = new THREE.Color( 0xffffff );
+/*scene.background = new THREE.Color( 0xffffff );*/
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer({
-  antialias: true
+  antialias: true,
+  alpha: true
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
